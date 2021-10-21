0cfc1d4dd6acc5032a841694135340baecda1e5a
kitsao <kitsaojackson22@gmail.com>
2021-10-21 16:22:11 +0300

    add separate api url for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+const baseURL =
+  window.location.hostname !== "localhost"
+    ? "https://booapi.onrender.com"
+    : "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: baseURL,
 });
 
 export default api;
