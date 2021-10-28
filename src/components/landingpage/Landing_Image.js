fafe5e0b81b3ed4468e9415429342388cea9abad
kitsao <kitsaojackson22@gmail.com>
2021-10-28 14:07:08 +0300

    add api image and margin

--- src/components/landingpage/Landing_Image.js
+++ src/components/landingpage/Landing_Image.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Landing_Image({ image_src }) {
   return (
-    <div className="relative lg:w-5/6 xl:w-3/5 lg:mx-auto shadow-lg border rounded-md lg:mb-12 overflow-hidden">
+    <div className="relative lg:w-5/6 xl:w-3/5 lg:mx-auto shadow-lg border rounded-md lg:mb-12 overflow-hidden mx-2">
       <img
         className=" w-full h-full object-cover"
         src={image_src}
